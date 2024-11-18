b592640c12230848e1090719733c70c10b0e372a
Seni0r-C <carlosluis199924@hotmail.com>
2024-11-18 08:34:36 -0500

    ya funciona el login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,13 +7,13 @@ exports.loginUser = async (req, res) => {
         const { email, password } = req.body;
 
         // Verifica si el usuario existe en la base de datos
-        const sql = "SELECT * FROM Usuario WHERE Email = '?'";
+        const sql = "SELECT * FROM Usuario WHERE Email = ?";
         const usuario = await db.query(sql, [email]);
 
-        if (usuario.length === 0) {
+        if (usuario[0].length === 0) {
             return res.status(400).json({ exito: false, mensaje: 'Usuario no encontrado' });
         }
-        const user = usuario[0];
+        const user = usuario[0][0];
         // Compara la contraseña
         const isMatch = await bcrypt.compare(password, user.contrasenia);
         if (!isMatch) {
@@ -47,19 +47,23 @@ exports.restablecerPassword = async (req, res) => {
         const { email, password } = req.body;
 
         // Verifica si el usuario existe en la base de datos
-        let sql = "SELECT * FROM Usuario WHERE Email = '?'";
+        let sql = "SELECT * FROM Usuario WHERE Email = ?";
         const usuario = await db.query(sql, [email]);
 
-        if (usuario.length === 0) {
+        if (usuario[0].length === 0) {
             return res.status(400).json({ exito: false, mensaje: 'Usuario no encontrado' });
         }
-        const user = usuario[0];
+        const user = usuario[0][0];
 
         // Cambia la contraseña
         const salt = await bcrypt.genSalt(10);
         const newPassword = await bcrypt.hash(password, salt);
-        sql = "UPDATE Usuario SET Contrasenia = ? WHERE Email = ?";
+        sql = "UPDATE usuario SET contrasenia = ? WHERE email = ?";
         const result = await db.query(sql, [newPassword, user.email]);
+
+        if (result[0].affectedRows === 0) {
+            return res.status(400).json({ exito: false, mensaje: 'Error al cambiar la contraseña' });
+        }
 
         res.json({
             exito: true,
