1a7153f0e241e6056ea7d3b605792eb704b896e1
Carlos Arteaga <carlosluis199924@hotmail.com>
2024-11-19 21:15:06 -0500

    ya esta mas simple la api

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 // Ruta de login con validación
-router.post('/login', authController.loginUser);
+router.post("/login", authController.loginUser);
 router.post('/reset-password', authController.restablecerPassword);
 
 module.exports = router;
