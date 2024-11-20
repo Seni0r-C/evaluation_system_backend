2cdc63853edd48aa6bf2c56c028370c6a1c2c8ca
Carlos Arteaga <carlosluis199924@hotmail.com>
2024-11-19 21:07:07 -0500

    extio o no en la respuesta de la verificaicon del token

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const verifyToken = (req, res, next) => {
     const token = authHeader && authHeader.split(' ')[1]; // Formato esperado: "Bearer <token>"
 
     if (!token) {
-        return res.status(403).json({ mensaje: 'Acceso denegado. No se encontró el token.' });
+        return res.status(403).json({ exito: false, mensaje: 'Acceso denegado. No se encontró el token.' });
     }
 
     try {
@@ -13,7 +13,7 @@ const verifyToken = (req, res, next) => {
         req.user = decoded; // Almacenar los datos decodificados en la solicitud
         next(); // Continuar con la siguiente función middleware
     } catch (err) {
-        res.status(401).json({ mensaje: 'Token no válido' });
+        res.status(401).json({ exito: false, mensaje: 'Token no válido' });
     }
 };
 
