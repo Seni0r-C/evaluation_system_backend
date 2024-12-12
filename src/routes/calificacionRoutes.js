da262c3c881756d5ded0591bba246a798e72b694
Seni0r-C <carlosluis199924@hotmail.com>
2024-12-12 16:32:36 -0500

    endpoins documentados

--- src/routes/calificacionRoutes.js
+++ src/routes/calificacionRoutes.js
@@ -37,3 +37,634 @@ router.put('/rubrica-evaluacion/:id', calificacionController.updateRubricaEvalua
 
 // Exportar las rutas
 module.exports = router;
+
+// Rutas para tipo_evaluacion
+/**
+ * @swagger
+ * tags:
+ *   - name: Tipo Evaluacion
+ *     description: Operaciones relacionadas con los tipos de evaluación
+ */
+
+/**
+ * @swagger
+ * /tipo-evaluacion:
+ *   post:
+ *     summary: Crear un nuevo tipo de evaluación
+ *     tags: [Tipo Evaluacion]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/TipoEvaluacion'
+ *     responses:
+ *       201:
+ *         description: Tipo de evaluación creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TipoEvaluacion'
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /tipo-evaluacion/{id}:
+ *   get:
+ *     summary: Obtener un tipo de evaluación por ID
+ *     tags: [Tipo Evaluacion]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del tipo de evaluación a obtener
+ *     responses:
+ *       200:
+ *         description: Tipo de evaluación encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TipoEvaluacion'
+ *       404:
+ *         description: Tipo de evaluación no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /tipo-evaluacion/{id}:
+ *   put:
+ *     summary: Actualizar un tipo de evaluación por ID
+ *     tags: [Tipo Evaluacion]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del tipo de evaluación a actualizar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/TipoEvaluacion'
+ *     responses:
+ *       200:
+ *         description: Tipo de evaluación actualizado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TipoEvaluacion'
+ *       404:
+ *         description: Tipo de evaluación no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /tipo-evaluacion/{id}:
+ *   delete:
+ *     summary: Eliminar un tipo de evaluación por ID
+ *     tags: [Tipo Evaluacion]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del tipo de evaluación a eliminar
+ *     responses:
+ *       204:
+ *         description: Tipo de evaluación eliminado exitosamente
+ *       404:
+ *         description: Tipo de evaluación no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+
+// Rutas para rubrica
+/**
+ * @swagger
+ * tags:
+ *   - name: Rubrica
+ *     description: Operaciones relacionadas con las rubricas
+ */
+
+/**
+ * @swagger
+ * /rubrica:
+ *   post:
+ *     summary: Crear una nueva rubrica
+ *     tags: [Rubrica]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Rubrica'
+ *     responses:
+ *       201:
+ *         description: Rubrica creada exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Rubrica'
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica:
+ *   get:
+ *     summary: Obtener todas las rubricas
+ *     tags: [Rubrica]
+ *     responses:
+ *       200:
+ *         description: Lista de rubricas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Rubrica'
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica/{id}:
+ *   get:
+ *     summary: Obtener una rubrica por ID
+ *     tags: [Rubrica]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la rubrica a obtener
+ *     responses:
+ *       200:
+ *         description: Rubrica encontrada
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Rubrica'
+ *       404:
+ *         description: Rubrica no encontrada
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica/{id}:
+ *   put:
+ *     summary: Actualizar una rubrica por ID
+ *     tags: [Rubrica]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la rubrica a actualizar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Rubrica'
+ *     responses:
+ *       200:
+ *         description: Rubrica actualizada exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Rubrica'
+ *       404:
+ *         description: Rubrica no encontrada
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica/{id}:
+ *   delete:
+ *     summary: Eliminar una rubrica por ID
+ *     tags: [Rubrica]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la rubrica a eliminar
+ *     responses:
+ *       204:
+ *         description: Rubrica eliminada exitosamente
+ *       404:
+ *         description: Rubrica no encontrada
+ *       500:
+ *         description: Error en el servidor
+ */
+
+// Rutas para rubrica_criterio
+/**
+ * @swagger
+ * tags:
+ *   - name: Rubrica Criterio
+ *     description: Operaciones relacionadas con los criterios de rubricas
+ */
+
+/**
+ * @swagger
+ * /rubrica-criterio:
+ *   post:
+ *     summary: Crear un nuevo criterio de rubrica
+ *     tags: [Rubrica Criterio]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RubricaCriterio'
+ *     responses:
+ *       201:
+ *         description: Criterio de rubrica creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RubricaCriterio'
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-criterio:
+ *   get:
+ *     summary: Obtener todas los criterios de rubrica
+ *     tags: [Rubrica Criterio]
+ *     responses:
+ *       200:
+ *         description: Lista de criterios de rubrica
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/RubricaCriterio'
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-criterio/{id}:
+ *   get:
+ *     summary: Obtener un criterio de rubrica por ID
+ *     tags: [Rubrica Criterio]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del criterio de rubrica a obtener
+ *     responses:
+ *       200:
+ *         description: Criterio de rubrica encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RubricaCriterio'
+ *       404:
+ *         description: Criterio de rubrica no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-criterio/{id}:
+ *   put:
+ *     summary: Actualizar un criterio de rubrica por ID
+ *     tags: [Rubrica Criterio]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del criterio de rubrica a actualizar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RubricaCriterio'
+ *     responses:
+ *       200:
+ *         description: Criterio de rubrica actualizado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RubricaCriterio'
+ *       404:
+ *         description: Criterio de rubrica no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-criterio/{id}:
+ *   delete:
+ *     summary: Eliminar un criterio de rubrica
+ *     tags: [Rubrica Criterio]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del criterio de rubrica a eliminar
+ *     responses:
+ *       204:
+ *         description: Criterio de rubrica eliminado exitosamente
+ *       404:
+ *         description: Criterio de rubrica no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+
+// Rutas para rubrica_nivel
+/**
+ * @swagger
+ * tags:
+ *   - name: Rubrica Nivel
+ *     description: Operaciones relacionadas con los niveles de una rubrica
+ */
+
+/**
+ * @swagger
+ * /rubrica-nivel:
+ *   post:
+ *     summary: Crear un nuevo nivel de una rubrica
+ *     tags: [Rubrica Nivel]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RubricaNivel'
+ *     responses:
+ *       201:
+ *         description: Nivel de rubrica creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RubricaNivel'
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-nivel:
+ *   get:
+ *     summary: Obtener todas los niveles de una rubrica
+ *     tags: [Rubrica Nivel]
+ *     responses:
+ *       200:
+ *         description: Lista de niveles de rubrica
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/RubricaNivel'
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-nivel/{id}:
+ *   get:
+ *     summary: Obtener un nivel de una rubrica por ID
+ *     tags: [Rubrica Nivel]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del nivel de rubrica a obtener
+ *     responses:
+ *       200:
+ *         description: Nivel de rubrica encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RubricaNivel'
+ *       404:
+ *         description: Nivel de rubrica no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-nivel/{id}:
+ *   put:
+ *     summary: Actualizar un nivel de una rubrica por ID
+ *     tags: [Rubrica Nivel]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del nivel de rubrica a actualizar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RubricaNivel'
+ *     responses:
+ *       200:
+ *         description: Nivel de rubrica actualizado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RubricaNivel'
+ *       404:
+ *         description: Nivel de rubrica no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-nivel/{id}:
+ *   delete:
+ *     summary: Eliminar un nivel de una rubrica por ID
+ *     tags: [Rubrica Nivel]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del nivel de rubrica a eliminar
+ *     responses:
+ *       204:
+ *         description: Nivel de rubrica eliminado exitosamente
+ *       404:
+ *         description: Nivel de rubrica no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+
+// Rutas para rubrica_evaluacion
+/**
+ * @swagger
+ * tags:
+ *   - name: Rubrica Evaluacion
+ *     description: Operaciones relacionadas con las evaluaciones de una rubrica
+ */
+
+/**
+ * @swagger
+ * /rubrica-evaluacion:
+ *   post:
+ *     summary: Crear una nueva evaluación de una rubrica
+ *     tags: [Rubrica Evaluacion]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RubricaEvaluacion'
+ *     responses:
+ *       201:
+ *         description: Evaluación de rubrica creada exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RubricaEvaluacion'
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-evaluacion:
+ *   get:
+ *     summary: Obtener todas las evaluaciones de rubrica
+ *     tags: [Rubrica Evaluacion]
+ *     responses:
+ *       200:
+ *         description: Lista de evaluaciones de rubrica
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/RubricaEvaluacion'
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-evaluacion/{id}:
+ *   get:
+ *     summary: Obtener una evaluación de rubrica por ID
+ *     tags: [Rubrica Evaluacion]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la evaluación de rubrica a obtener
+ *     responses:
+ *       200:
+ *         description: Evaluación de rubrica encontrada
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RubricaEvaluacion'
+ *       404:
+ *         description: Evaluación de rubrica no encontrada
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-evaluacion/{id}:
+ *   put:
+ *     summary: Actualizar una evaluación de rubrica por ID
+ *     tags: [Rubrica Evaluacion]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la evaluación de rubrica a actualizar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RubricaEvaluacion'
+ *     responses:
+ *       200:
+ *         description: Evaluación de rubrica actualizada exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RubricaEvaluacion'
+ *       404:
+ *         description: Evaluación de rubrica no encontrada
+ *       500:
+ *         description: Error en el servidor
+ */
+
+/**
+ * @swagger
+ * /rubrica-evaluacion/{id}:
+ *   delete:
+ *     summary: Eliminar una evaluación de rubrica por ID
+ *     tags: [Rubrica Evaluacion]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la evaluación de rubrica a eliminar
+ *     responses:
+ *       204:
+ *         description: Evaluación de rubrica eliminada exitosamente
+ *       404:
+ *         description: Evaluación de rubrica no encontrada
+ *       500:
+ *         description: Error en el servidor
+ */
