14c7df78f4d458cd7bf6d0c19976aaefe0a8d57c
Carlos Arteaga <carlosluis199924@hotmail.com>
2025-01-13 01:33:23 -0500

    Actualizar controlador de autenticación para usar externAuth sin forzar el parámetro 'true'

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const db = require('../config/db');
 const axios = require('axios');
@@ -47,108 +46,6 @@ async function utmAuth(body) {
     return await apiData;
 }
 
-
-// Simulamos un conjunto de usuarios con sus contraseñas
-const usuariosSimulados = {
-    'vicedecano': {
-      nombres: 'KATTY GARCIA BARREIRO VERA',
-      tipo_usuario: 'VICEDECANO',
-      idpersonal: 12342,
-      datos_estudio: JSON.stringify([
-          { carrera: 'Ingenieria En Sistemas Informaticos', facultad: 'CIENCIAS INFORMÁTICAS' }
-        ])
-    },
-    'tribunal': {
-        nombres: 'PEDRO MANOLO ANESTECIO ONETWO',
-        tipo_usuario: 'DOCENTE',
-        idpersonal: 12351,
-        datos_estudio: JSON.stringify([
-            { carrera: 'Ingenieria En Sistemas Informaticos', facultad: 'CIENCIAS INFORMÁTICAS' }
-      ])
-    },
-    // Estudiantes
-    'pupilo': {
-        nombres: 'SANTIAGO SEGUNDO PACHECO VEREDICTO',
-        tipo_usuario: 'ESTUDIANTE',
-        idpersonal: 34351,
-        datos_estudio: JSON.stringify([
-            { carrera: 'Ingenieria En Sistemas Informaticos', facultad: 'CIENCIAS INFORMÁTICAS' }
-      ])
-    },
-    'alumno': {
-      nombres: 'JAIME ENRIQUYE ALMIGUEZ GONZALEZ',
-      tipo_usuario: 'ESTUDIANTE',
-      idpersonal: 19359,
-      datos_estudio: JSON.stringify([
-        { carrera: 'Ingenieria En Sistemas Informaticos', facultad: 'CIENCIAS INFORMÁTICAS' }
-      ])
-    },
-    'alumna': {
-      nombres: 'AGUINALDA GUISELLE VALIVIEZO JILIWE',
-      tipo_usuario: 'ESTUDIANTE',
-      idpersonal: 19355,
-      datos_estudio: JSON.stringify([
-        { carrera: 'Ingenieria En Sistemas Informaticos', facultad: 'CIENCIAS INFORMÁTICAS' }
-      ])
-    },
-    // Tutores
-    'estudiante': {
-        nombres: 'TAMIÑAWI SUMI SUMIWKA MANIKO',
-        tipo_usuario: 'DOCENTE',
-        idpersonal: 19351,
-        datos_estudio: JSON.stringify([
-            { carrera: 'Ingenieria En Sistemas Informaticos', facultad: 'CIENCIAS INFORMÁTICAS' }
-      ])
-    },
-    'tutor': {
-        nombres: 'CARLOS MANICHO VENEZUELO MANGIZO',
-        tipo_usuario: 'ESTUDIANTE',
-        idpersonal: 56709,
-        datos_estudio: JSON.stringify([
-          { carrera: 'Ingenieria En Sistemas Informaticos', facultad: 'CIENCIAS INFORMÁTICAS' }
-        ])
-      },
-    'tutora': {
-      nombres: 'ANA GABRIELA YUKATAN SLOVAKY',
-      tipo_usuario: 'DOCENTE',
-      idpersonal: 19360,
-      datos_estudio: JSON.stringify([
-        { carrera: 'Ingenieria En Sistemas Informaticos', facultad: 'CIENCIAS INFORMÁTICAS' }
-      ])
-    },
-    'carlos': {
-      nombres: 'ANA GABRIELA YUKATAN SLOVAKY',
-      tipo_usuario: 'ADMINISTRADOR',
-      idpersonal: 19360,
-      datos_estudio: JSON.stringify([
-        { carrera: 'Ingenieria En Sistemas Informaticos', facultad: 'CIENCIAS INFORMÁTICAS' }
-      ])
-    }
-    // Otros usuarios pueden ser añadidos aquí para la simulación
-  };
-  
-  // La función `truchaAuth` emula una llamada a la API externa
-  const truchaAuth = async ({ usuario, clave }) => {
-    // Simulamos un retraso en la respuesta como si fuera una llamada externa
-    await new Promise(resolve => setTimeout(resolve, 500));
-  
-    // Verificamos que el usuario exista y que la contraseña sea la correcta
-    if (usuariosSimulados[usuario] && clave === '123') {
-      // Si existe el usuario y la contraseña es correcta, devolvemos los datos del usuario
-      return usuariosSimulados[usuario];
-    } else {
-      // Si no, lanzamos un error indicando que el usuario o la contraseña son incorrectos
-      throw new Error('Usuario o contraseña incorrectos');
-    }
-  };
-  
-async function externAuth(body, truchaMode=false) {
-    if(truchaMode) {
-        return await truchaAuth(body);
-    }
-    return await utmAuth(body);
-}
-
 exports.loginUser = async (req, res) => {
     try {
         const { usuario, password } = req.body;
@@ -158,7 +55,7 @@ exports.loginUser = async (req, res) => {
             clave: password // Usa la contraseña proporcionada
         };
 
-        const apiData = await externAuth(body);
+        const apiData = await utmAuth(body);
 
         // Verifica si el usuario existe en la base de datos
         const sql = "SELECT * FROM usuario WHERE usuario = ?";
@@ -267,76 +164,6 @@ async function insertCarreraIfNotExists(nombreCarrera) {
     }
     return carrera[0].id;
 }
-
-exports.restablecerPassword = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        // Verifica si el usuario existe en la base de datos
-        let sql = "SELECT * FROM usuario WHERE Email = ?";
-        const [usuario] = await db.query(sql, [email]);
-
-        if (usuario.length === 0) {
-            return res.status(400).json({ exito: false, mensaje: 'Usuario no encontrado' });
-        }
-        const user = usuario[0];
-
-        // Cambia la contraseña
-        const salt = await bcrypt.genSalt(10);
-        const newPassword = await bcrypt.hash(password, salt);
-        sql = "UPDATE usuario SET contrasenia = ? WHERE email = ?";
-        const [result] = await db.query(sql, [newPassword, user.email]);
-
-        if (result.affectedRows === 0) {
-            return res.status(400).json({ exito: false, mensaje: 'Error al cambiar la contraseña' });
-        }
-
-        res.json({
-            exito: true,
-            mensaje: 'Contraseña cambiada exitosamente'
-        });
-    } catch (error) {
-        res.status(500).json({
-            exito: false,
-            mensaje: 'Error del servidor',
-            error: error.message
-        });
-    }
-};
-
-exports.registerUser = async (req, res) => {
-    try {
-        const { nombre, apellido, email, password, id_rol } = req.body;
-
-        // Verifica si el email ya está registrado
-        const sqlCheck = "SELECT * FROM usuario WHERE Email = ?";
-        const [existingUser] = await db.query(sqlCheck, [email]);
-
-        if (existingUser.length > 0) {
-            return res.status(400).json({ exito: false, mensaje: 'El correo ya está registrado' });
-        }
-
-        // Cifra la contraseña
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Inserta el nuevo usuario en la base de datos
-        const sqlInsert = "INSERT INTO usuario (nombre, apellido, email, contrasenia, id_rol) VALUES (?, ?, ?, ?, ?)";
-        const [result] = await db.query(sqlInsert, [nombre, apellido, email, hashedPassword, id_rol]);
-
-        res.status(201).json({
-            exito: true,
-            mensaje: 'Usuario registrado exitosamente',
-            datos: { id: result.insertId, nombre, apellido, email, id_rol }
-        });
-    } catch (error) {
-        res.status(500).json({
-            exito: false,
-            mensaje: 'Error del servidor',
-            error: error.message
-        });
-    }
-};
 
 exports.getAuthenticatedUser = async (req, res) => {
     try {
