26bb3d0dcc197772eeea02f64e2069d2406c6487
Jostin Rodriguez <jarzta99@gmail.com>
2025-02-14 22:30:13 -0500

    Mejora en Gestión de criterios de rúbirca (hay que eliminar la otra sección que está incompleta)

--- src/controllers/rubricaController.js
+++ src/controllers/rubricaController.js
@@ -145,12 +145,34 @@ exports.obtenerTiposEvaluacionPorModalidad = async (req, res) => {
     const { modalidad_id } = req.params;
     try {
         const [tiposEvaluacion] = await db.execute(
-            `SELECT DISTINCT te.id, te.nombre
-                FROM sistema_tipo_evaluacion te
-                JOIN rubrica r ON te.id = r.tipo_evaluacion_id
-                WHERE r.modalidad_id = ?`,
+                `
+            SELECT
+
+                te.id, 
+                        CASE 
+                    WHEN te_padre.nombre IS NOT NULL AND ans.trabajo_modalidad_id=r.modalidad_id
+                    THEN CONCAT(te.nombre, ' (', te_padre.nombre, ')') 
+                    ELSE te.nombre 
+                END AS nombre,
+                    smt.nombre AS modaliad                    
+            FROM sistema_tipo_evaluacion te
+            JOIN rubrica r ON te.id = r.tipo_evaluacion_id
+            LEFT JOIN acta_notas_scheme ans ON te.id = ans.comp_id 
+            LEFT JOIN sistema_tipo_evaluacion te_padre ON ans.comp_parent_id = te_padre.id 
+            LEFT JOIN sistema_modalidad_titulacion smt ON smt.id = r.modalidad_id
+            WHERE r.modalidad_id = ?
+            AND te.id  NOT IN (SELECT DISTINCT comp_parent_id FROM acta_notas_scheme WHERE comp_parent_id IS NOT NULL)
+            GROUP BY te.id
+    `,
             [modalidad_id]
         );
+        // const [tiposEvaluacion] = await db.execute(
+        //     `SELECT DISTINCT te.id, te.nombre
+        //         FROM sistema_tipo_evaluacion te
+        //         JOIN rubrica r ON te.id = r.tipo_evaluacion_id
+        //         WHERE r.modalidad_id = ?`,
+        //     [modalidad_id]
+        // );
         res.json(tiposEvaluacion);
     } catch (error) {
         console.error("Error al obtener tipos de evaluación:", error);
