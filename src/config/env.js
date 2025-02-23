8c23eba5d85b6850a4eaf75acc862fe311662a3b
Carlos Arteaga <carlosluis199924@hotmail.com>
2025-02-23 14:02:42 -0500

    chore: update FRONT_URL in env.js to remove trailing slash

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 
 exports.PORT = process.env.PORT;
-exports.FRONT_URL = process.env.FRONT_URL??"http://localhost:5173/";
+exports.FRONT_URL = process.env.FRONT_URL??"http://localhost:5173";
