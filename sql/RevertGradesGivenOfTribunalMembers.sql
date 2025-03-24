809b025eec60c090e699c23963907c0e95f2da3c
Jostin Rodriguez <jarzta99@gmail.com>
2025-03-24 17:37:09 -0500

    Create RevertGradesGivenOfTribunalMembers.sql

---
+++ sql/RevertGradesGivenOfTribunalMembers.sql
@@ -0,0 +1,18 @@
+SELECT * FROM rubrica_evaluacion;
+-- removes records of grades on thesis work
+DELETE FROM rubrica_evaluacion WHERE trabajo_id = 10;
+
+
+-- 'CON TRIBUNAL'
+-- 'DEFENDIDO'
+SELECT te.id FROM trabajo_estado te WHERE te.nombre = 'CON TRIBUNAL';
+-- Change state of thesis work to regradement of tribunal members
+UPDATE trabajo_titulacion 
+SET estado_id = (SELECT te.id 
+						FROM trabajo_estado te 
+						WHERE te.nombre = 'CON TRIBUNAL') 
+						WHERE id = 10 AND estado_id = (
+												SELECT te.id 
+												FROM trabajo_estado te 
+												WHERE te.nombre = 'DEFENDIDO'
+												);
