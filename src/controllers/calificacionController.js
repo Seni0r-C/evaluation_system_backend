886515522865ecc08bb6a77dc6e5961e1c6df8c8
Jostin Rodriguez <jarzta99@gmail.com>
2025-04-03 07:52:31 -0500

    Filter of query apply for correct verification of tribunal member gradement

--- src/controllers/calificacionController.js
+++ src/controllers/calificacionController.js
@@ -227,7 +227,7 @@ const getCompleteThesisGradeStatement = () => {
 		    sistema_tipo_evaluacion te ON r.tipo_evaluacion_id = te.id
 		INNER JOIN 
 		    trabajo_titulacion tt ON re.trabajo_id = tt.id
-		WHERE tt.id = ? 
+		WHERE tt.id = ? AND te.nombre NOT LIKE "EXAMEN TEORICO"
 		GROUP BY docente.id
         `;
 }
--- src/services/tribunalMambersGradeService.js
+++ src/services/tribunalMambersGradeService.js
@@ -38,20 +38,7 @@ exports.GetTribunalMembersGradesService = async (trabajo_id) => {
     const [rows] = await db.query(`${getTribunalMembersGradesStatement()}`, [trabajo_id]);
     return rows;
 }
-
-// const getTribunalMembersByTrabajoIdStatement = () => {
-//     return `
-//         SELECT DISTINCT 
-//             u.id AS id, 
-//             u.nombre AS nombre 
-//         FROM 
-//             rubrica_evaluacion re
-//         INNER JOIN 
-//             usuario u ON re.docente_id = u.id
-//         WHERE 
-//             re.trabajo_id = ?
-//     `;
-// };
+ 
 
 const getTribunalMembersByTrabajoIdStatement = () => {
     return `
@@ -59,7 +46,17 @@ const getTribunalMembersByTrabajoIdStatement = () => {
             u.id AS id, 
             u.nombre AS nombre,
             CASE 
-                WHEN re.id IS NOT NULL THEN 'CALIFICADO' 
+                WHEN EXISTS (
+                    SELECT 1 
+                    FROM rubrica_evaluacion re2
+                    JOIN rubrica_criterio rc2 ON re2.rubrica_criterio_id = rc2.id
+                    JOIN rubrica r2 ON rc2.rubrica_id = r2.id
+                    JOIN sistema_tipo_evaluacion ste2 ON r2.tipo_evaluacion_id = ste2.id
+                    WHERE 
+                        re2.docente_id = tt.docente_id
+                        AND re2.trabajo_id = tt.trabajo_id
+                        AND ste2.nombre NOT LIKE '%EXAMEN TEORICO%'
+                ) THEN 'CALIFICADO' 
                 ELSE 'PENDIENTE' 
             END AS estado
         FROM 
@@ -71,9 +68,10 @@ const getTribunalMembersByTrabajoIdStatement = () => {
             AND tt.docente_id = re.docente_id
         WHERE 
             tt.trabajo_id = ?
-        ORDER BY u.nombre
+        ORDER BY u.nombre;
     `;
-};  
+};
+
 
 exports.GetTribunalMembersByTrabajoIdService = async (trabajo_id) => {
     const [rows] = await db.query(`${getTribunalMembersByTrabajoIdStatement()}`, [trabajo_id]);
