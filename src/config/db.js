31553abac8b2f57ebc016f74d0352f42d651aa8e
Carlos Arteaga <carlosluis199924@hotmail.com>
2025-08-14 00:01:20 -0500

    Implement code changes to enhance functionality and improve performance

--- src/config/db.js
+++ src/config/db.js
@@ -8,4 +8,18 @@ const pool = mysql.createPool({
     database: DB_NAME,
 });
 
+// Cada vez que se crea una nueva conexión en el pool
+pool.on('connection', (connection) => {
+    connection.query(
+        "SET SESSION sql_mode = REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', '')",
+        (err) => {
+            if (err) {
+                console.error('Error al desactivar ONLY_FULL_GROUP_BY:', err);
+            } else {
+                console.log('ONLY_FULL_GROUP_BY desactivado para esta conexión');
+            }
+        }
+    );
+});
+
 module.exports = pool.promise();
